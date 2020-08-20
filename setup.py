53550c2864981f3d777dae0a96e2c4acefb7fb6a
amifunny <hemantsingh.buenodev@gmail.com>
2020-08-20 13:12:04 +0530

    Fix package readme

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    complete_readme = fh.read()
+    long_description = complete_readme.split("**System image")[0]
+    long_description += "\n\n**Made by Help-a-Sloth org. Check us on GitHub.**"
+
+
 setuptools.setup(
     name="mischief-managed", 
     packages=setuptools.find_packages(),
@@ -10,6 +16,7 @@ setuptools.setup(
     description=("Files outside any folder are "+
                 "made tidy/managed by putting inside folder based on "+
                 "their extension or date"),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Help-a-Sloth/mischief-managed",
     maintainer="amifunny",
@@ -21,7 +28,7 @@ setuptools.setup(
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ]
 
